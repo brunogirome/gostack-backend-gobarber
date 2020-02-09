73701d4866c03a4189e6066c3767022565fce27b
Bruno Girome <brunogirome@gmail.com>
2020-02-09 13:56:53 -0300

    Configuração do mongoose e criação de notifications

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,12 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+// Biblioteca de língua portuguesa do date-fns
+import pt_BR from 'date-fns/locale/pt-BR';
 
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -98,6 +101,24 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    // Notificando o provider
+    const user = await User.findByPk(req.userId);
+    // Criando data customizada
+    // Neste caso, os dados que estão entre aspas simples (como o 'dia'), será
+    // transcrito como a pópria palavra. dd é o dia, MMMM é o mês por extenso,
+    // H hora e mm minutos
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      // Lembrando que esse pt é um import separado do date-fns
+      { locale: pt_BR }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
---
+++ src/app/schemas/Notification.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const NotificationSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Number,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// Nome da collection do banco, Schema do db
+export default mongoose.model('Notification', NotificationSchema);
